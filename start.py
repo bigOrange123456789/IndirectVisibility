31957c29ca44998b82f4c806de20619ce3d06dda
bigOrange123456789 <74175180+bigOrange123456789@users.noreply.github.com>
2022-08-30 00:48:29 +0800

    修补了一个参数设置上的BUG

--- start.py
+++ start.py
@@ -72,6 +72,7 @@ class IndirectVisibility:
     self.nameList=nameList
     return ls
   def start(self):
+    self.optSet("out")
     t0=t.time()
     print("opt")
     for i in self.opt:
@@ -100,27 +101,27 @@ class IndirectVisibility:
     print(ls)
     for i in range(len(self.nameList)):
       self.ls[self.nameList[i]]=ls[i]
-if __name__ == "__main__":#用于测试
-    print('version:2022.08.28-1')
-    # iv=IndirectVisibility({"in":"in/test"})
-    iv=IndirectVisibility({"in":"in/test_sort"})
-    iv.opt["sim"]=False#True#
-    iv.opt["step"]=1
-    iv.opt["useGPU"]=False
-    iv.opt["step_component"]=1
-    iv.opt["groups_outEachStep"]=True
-    iv.start()
 # if __name__ == "__main__":#用于测试
 #     print('version:2022.08.28-1')
 #     # iv=IndirectVisibility({"in":"in/test"})
-#     iv=IndirectVisibility({"in":"in/test_component2_multidirection"})
+#     iv=IndirectVisibility({"in":"in/test_sort"})
 #     iv.opt["sim"]=False#True#
 #     iv.opt["step"]=1
 #     iv.opt["useGPU"]=False
-#     iv.opt["step_component"]=2
+#     iv.opt["step_component"]=1
 #     iv.opt["groups_outEachStep"]=True
-#     iv.opt["multidirectionalSampling"]=True
 #     iv.start()
+if __name__ == "__main__":#用于测试
+    print('version:2022.08.28-1')
+    # iv=IndirectVisibility({"in":"in/test"})
+    iv=IndirectVisibility({"in":"in/test_component2_multidirection"})
+    iv.opt0["sim"]=False#True#
+    iv.opt0["step"]=1
+    iv.opt0["useGPU"]=False
+    iv.opt0["step_component"]=2
+    iv.opt0["groups_outEachStep"]=True
+    iv.opt0["multidirectionalSampling"]=True
+    iv.start()
 if False:#用于测试
     print('version:2022.08.28-1')
     # iv=IndirectVisibility({"in":"in/test"})
