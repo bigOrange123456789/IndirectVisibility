8e407e46232294dec79a4270b4fc69297e7951d9
bigOrange123456789 <74175180+bigOrange123456789@users.noreply.github.com>
2022-08-29 19:45:49 +0800

    save

--- start.py
+++ start.py
@@ -17,33 +17,38 @@ class IndirectVisibility:
     import os
     if not os.path.exists(path):
         os.makedirs(path) 
-  def __init__(self,opt):
-      self.mkdir("out")
+  def setOpt(self):
+      out=self.outPath
       self.opt={
         "in":"./in/test.json",
-        "out1":"./out/1.direct",#直接可见度矩阵，txt
-        "out2":"./out/2.redunList.json",#冗余视点列表
-        "out2.d0":"./out/2.d0",
-        "out2.groups_arr":"./out/2.groups_arr",
+        "out1":"./"+out+"/1.direct",#直接可见度矩阵，txt
+        "out2":"./"+out+"/2.redunList.json",#冗余视点列表
+        "out2.d0":"./"+out+"/2.d0",
+        "out2.groups_arr":"./"+out+"/2.groups_arr",
         "groups_outEachStep":False,#是否输出距离过程中每一次迭代的结果
-        "out2.nameList":"./out/2.nameList",
+        "out2.nameList":"./"+out+"/2.nameList",
         "step":2,#聚类个数的步长
         "step_component":1,#构件聚类个数的步长
         "useGPU":True,#是否使用GPU
-        "out3":"./out/3.e",#特征矩阵，txt
-        "out4":"./out/4.simMat",#视觉相关度矩阵，txt
-        "out5":"./out/5.d1",#间接相关度矩阵，txt
-        "out6":"./out/6.ls",#资源加载列表，txt.json
-        "out6_d":"./out/6.ls_d",#资源加载列表，txt.json
+        "out3":"./"+out+"/3.e",#特征矩阵，txt
+        "out4":"./"+out+"/4.simMat",#视觉相关度矩阵，txt
+        "out5":"./"+out+"/5.d1",#间接相关度矩阵，txt
+        "out6":"./"+out+"/6.ls",#资源加载列表，txt.json
+        "out6_d":"./"+out+"/6.ls_d",#资源加载列表，txt.json
         "sim":False,#True,#是否简化计算流程 #简化流程后只计算直接可见度，不计算间接可见度
-        "out6_i":"./out/6.ls_i",#资源加载列表，txt.json
-        "out7_d":"./out/7.ls_d",
+        "out6_i":"./"+out+"/6.ls_i",#资源加载列表，txt.json
+        "out7_d":"./"+out+"/7.ls_d",
         "areaMin":64,#构件的投影面积小于这个数值视为不可见
         "multidirectionalSampling":False,#True,#不同方向的采样结果分开存储
         'startNow':False,#是否在对象初始化阶段执行start()方法
         }
-      for i in opt:
-          self.opt[i]=opt[i]
+      for i in self.opt0:
+          self.opt[i]=self.opt0[i]
+  def __init__(self,opt0):
+      self.mkdir("out")
+      self.opt0=opt0
+      self.outPath="out"
+      self.setOpt()
       if self.opt['startNow']==True:
         self.start()
   def process(self,d0,d0_,nameList0,nameList,redunList):
@@ -57,14 +62,11 @@ class IndirectVisibility:
     ls,ls1,ls2,nameList,t6=Lists(self.opt,d0,d1,redunList,nameList).result
     print('7.缩小误差')
     ReduceError(self.opt,d0_,nameList0,ls1,nameList).result#self.reduceError(d0_,nameList0,ls1,nameList)
-
     print("step3.执行时间："+str((t3/60/1000))+" min")
     print("step4.执行时间："+str((t4/60/1000))+" min")
     print("step5.执行时间："+str((t5/60/1000))+" min")
     print("step6.执行时间："+str((t6/60/1000))+" min")
-
     return ls
-
   def start(self):
     t0=t.time()
     print("opt")
