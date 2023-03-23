a34bc0b239d7f3a7f9c72a173b77e47e61891c36
bigOrange123456789 <bigOrange123456789>
2023-03-23 15:04:17 +0800

    save

--- a_sampling/renderLib/Mesh0.py
+++ a_sampling/renderLib/Mesh0.py
@@ -98,13 +98,7 @@ class Mesh0():#实例化网格
         for matrix in matrices:
             vertex0,face0=getVF(mesh,matrix)
             
-            print()
-            print(id)
-            print("顶点个数",vertex0.shape)
-            print("min",np.min(vertex0,0))
-            print("max",np.max(vertex0,0))
-            print()
-
+            
             # print("vertex0",vertex0)
             # print("face0",face0)
             start_pos=vertex_all.shape[0]
@@ -121,8 +115,8 @@ class Mesh0():#实例化网格
             (id&0x00ff00)>>8,#g
             (id&0x0000ff)#r
             ])/255
-        if len(matrices)>1:
-            exit(0)
+        # if len(matrices)>1:
+        #     exit(0)
         # color=[0,1,0]#用于测试
         for i in range(3):
             vertex_all=np.c_[vertex_all,color[i]*np.ones(vertex_all.shape[0])]
